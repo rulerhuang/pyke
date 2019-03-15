f418c1dfab47cc04846c5567b3e63ebd71fef1d0
xushouming <xushouming@geetest.com>
2019-03-15 15:33:33 +0800

    refactor

--- handlers/index.go
+++ handlers/index.go
@@ -1,10 +1,18 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"pyke/config"
 )
 
 func Index(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"msg": "hello, pyke is running!"})
+	host, port, mode :=
+		config.ConfigInstant.Host,
+		config.ConfigInstant.Port,
+		config.ConfigInstant.Mode
+
+	msg := fmt.Sprintf("hello, pyke is running at %s:%d, at %s mode!", host, port, mode)
+	c.JSON(http.StatusOK, gin.H{"msg": msg})
 }
